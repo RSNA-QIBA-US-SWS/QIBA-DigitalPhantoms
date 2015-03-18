1118e8f7c853669b12af28b2511949d107235177
Mark Palmeri <mlp6@duke.edu>
2015-03-18 09:26:06 -0400

    WIP

--- field/genLoads.m
+++ field/genLoads.m
@@ -7,13 +7,13 @@ function genLoads(FNUM, FOCDEPTH, FREQ, ALPHA)
 % mlp6@duke.edu
 % 2015-03-17
 
-nodeFile = sprinf('../mesh/nodesFoc%.fmm.dyn', FOCDEPTH/100);
+nodeFile = sprintf('../mesh/nodesFoc%.fmm.dyn', FOCDEPTH*1000);
 
 inputIntensityFile = field2dyna(nodeFile, ALPHA, FNUM, [0 0 FOCDEPTH], FREQ, ...
                                 'curvlin_qiba', 'gaussian');
 
-normIntensityFile = 'dyna-I-f3.00-F2.0-FD0.07-a0.45.mat';
+normIntensityFile = 'dyna-I-f3.00-F2.0-FD0.070-a0.45.mat';
 
-makeLoadsTemps(inputIntensityFile,
-               normIntensityFile,
+makeLoadsTemps(inputIntensityFile, ...
+               normIntensityFile, ...
                1000, 500, 2.0, 0.025^3, 'q', 1);
