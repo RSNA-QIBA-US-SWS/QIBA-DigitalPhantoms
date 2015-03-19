85411a3a9036f187e8ceafbf755f1f41a310b625
Mark Palmeri <mlp6@duke.edu>
2015-03-19 16:34:13 -0400

    fix syntax for nodeLoad*.vts generation

--- field/genLoads.m
+++ field/genLoads.m
@@ -5,9 +5,11 @@ function genLoads(FNUM, FOCDEPTH, FREQ, ALPHA)
 %
 % Mark Palmeri
 % mlp6@duke.edu
-% 2015-03-17
+% 2015-03-19
 
-nodeFile = sprintf('../mesh/nodesFoc%.fmm.dyn', FOCDEPTH*1000);
+FDmm = FOCDEPTH * 1000;
+
+nodeFile = sprintf('../mesh/nodesFoc%.fmm.dyn', FDmm);
 
 inputIntensityFile = field2dyna(nodeFile, ALPHA, FNUM, [0 0 FOCDEPTH], FREQ, ...
                                 'curvlin_qiba', 'gaussian');
@@ -17,5 +19,8 @@ normIntensityFile = 'dyna-I-f3.00-F2.0-FD0.070-a0.45.mat';
 makeLoadsTemps(inputIntensityFile, ...
                normIntensityFile, ...
                1000, 500, 2.0, 0.025^3, 'q', 1);
+
+% generate VTU files for paraview
+system(sprintf('python ~/git/fem/mesh/create_pointloads_vtk.py --nodefile ../mesh/nodesFoc%imm.dyn --loadfile PointLoads-f3.00-F%.1f-FD0.0%i-a0.45.dyn --loadout nodeLoadsFoc%immF%.1f', FDmm, FNUM, FDmm, FDmm, FNUM));
 
 quit
