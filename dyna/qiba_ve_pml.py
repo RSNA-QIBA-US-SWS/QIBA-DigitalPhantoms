478c18c52904e2f349b0a62b3daa9a64c5d30c28
Mark Palmeri <mlp6@duke.edu>
2015-07-20 22:21:18 -0400

    WIP: VE Phase 2 phantom setup

---
+++ dyna/qiba_ve_pml.py
@@ -0,0 +1,73 @@
+"""
+qiba_ve_pml.py - setup SGE scripts to launch sims on the cluster
+
+Viscoelastic digital phantoms for the QIBA industry comparison.
+
+Mark Palmeri
+mark.palmeri@duke.edu
+2015-06-28
+"""
+
+import os
+from math import sqrt as sqrt
+
+G0 = [X, X, X]  # kPa
+GI = [X, X, X]  # kPa
+ETA = [X, X, X]
+YoungsModuli = [x*3.0 for x in G0]  # kPa
+ExcitationDurations = [167, 334]  # us, 500 and 1000 cycles @ 3 MHz
+FocalDepths = [30, 50, 70]  # mm
+Fnums = [2.0, 3.5]
+
+root = '/getlab/mlp6/scratch/QIBA-DigitalPhantoms'
+femgit = '/home/mlp6/projects/fem'
+indyn = 'qiba_ve_pml.dyn'
+sgeFile = 'qiba_ve_pml.sh'
+
+for YM in YoungsModuli:
+    for FD in FocalDepths:
+        for ED in ExcitationDurations:
+            for FN in Fnums:
+
+                sim_path = '%s/data/E%.1fkPa/foc%imm/F%.1f/EXCDUR_%ius/' % \
+                           (root, YM, FD, FN, ED)
+
+                if not os.path.exists(sim_path):
+                    os.makedirs(sim_path)
+
+                os.chdir(sim_path)
+                print(os.getcwd())
+
+                if not os.path.exists('res_sim.mat'):
+                    print('\tres_sim.mat missing . . . running ls-dyna')
+                    os.system('cp %s/dyna/%s .' % (root, indyn))
+                    os.system("sed -i -e 's/YM/%.1f/' %s" %
+                              (YM * 10000.0, indyn)
+                              )
+                    os.system("sed -i -e 's/TOFF1/%.1f/' %s" %
+                              (ED, indyn)
+                              )
+                    os.system("sed -i -e 's/TOFF2/%.1f/' %s" %
+                              (ED + 1, indyn)
+                              )
+                    os.system("sed -i -e 's/TRUN/%i/' %s" %
+                              (25 / sqrt(YM/3), indyn)
+                              )
+                    os.system("ln -fs %s/field/PointLoads-f3.00-"
+                              "F%.1f-FD0.0%i-a0.45.dyn loads.dyn" %
+                              (root, FN, FD)
+                              )
+                    os.system("ln -fs %s/mesh/nodesFoc%imm.dyn nodes.dyn" %
+                              (root, FD)
+                              )
+                    os.system("ln -fs %s/mesh/elemsFoc%imm_pml.dyn "
+                              "elems_pml.dyn" % (root, FD)
+                              )
+                    os.system("ln -fs %s/mesh/bcPMLfoc%imm.dyn bc_pml.dyn" %
+                              (root, FD)
+                              )
+                    os.system('cp %s/dyna/%s .' % (root, sgeFile))
+
+                    os.system('sbatch %s' % (sgeFile))
+                else:
+                    print('res_sim.mat already exists')
